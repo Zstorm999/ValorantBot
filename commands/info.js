0457913f052181b11fba6339b446196959c0cc06
Zstorm999 <thomasarchambeau@outlook.fr>
2022-07-07 11:04:27 +0200

    Added info command

---
+++ commands/info.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('info')
+        .setDescription('Some info about me'),
+    async execute(interaction) {
+        await interaction.reply('Hello, I’m Valorant End Game bot !\n \
+            I was made by Zstorm, you can find my source code here: \
+            https://github.com/Zstorm999/ValorantBot\n \
+            My current version is `0.2`');
+    }
+}
